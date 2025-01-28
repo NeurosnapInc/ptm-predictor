ee2a8f89964c5fd26a2400761a84db5a4133d52e
Keaun A <kja.working@gmail.com>
2025-01-28 15:59:21 -0500

    better code for processing and cleaning dataset, added better logging, better comments

--- fetch_data.py
+++ fetch_data.py
@@ -6,17 +6,23 @@ from time import sleep
 import bs4
 import pandas as pd
 import requests
+from neurosnap.log import logger
 from neurosnap.protein import fetch_uniprot
 from tqdm import tqdm
 
-### Functions
-# TODO
+### Constants
+INPUTS_RAW_DIR = "ptms_raw"
+INPUTS_CLEAN_DIR = "ptms_clean"
 
-# prepare download dir
-OUTPUT_DIR = "ptms"
-shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
-os.makedirs(OUTPUT_DIR)
+### Core Program
+## prepare directories
+shutil.rmtree(INPUTS_RAW_DIR, ignore_errors=True)
+os.makedirs(INPUTS_RAW_DIR)
 
+shutil.rmtree(INPUTS_CLEAN_DIR, ignore_errors=True)
+os.makedirs(INPUTS_CLEAN_DIR)
+
+## download raw data
 # fetch and parse download page
 r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/download.php")
 r.raise_for_status()
@@ -26,10 +32,10 @@ soup = bs4.BeautifulSoup(r.text)
 for el in soup.select("#site .btn.btn-primary"):
   if el.text == "MAC / Linux":
     name = el["href"].split("/")[-1]
-    print(f"[+] Fetching {name}")
+    logger.info(f"Fetching {name}")
     r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/" + el["href"])
     r.raise_for_status()
-    tgz_path = os.path.join(OUTPUT_DIR, name)
+    tgz_path = os.path.join(INPUTS_RAW_DIR, name)
 
     # Save the .tgz file
     with open(tgz_path, "wb") as f:
@@ -37,14 +43,17 @@ for el in soup.select("#site .btn.btn-primary"):
 
     # Extract the .tgz file
     with tarfile.open(tgz_path, "r:gz") as tar:
-      tar.extractall(OUTPUT_DIR)
+      tar.extractall(INPUTS_RAW_DIR)
 
     os.remove(tgz_path)
 
+## process each raw TSV file
 # add full uniprot sequences to each dataframe
-for fname in os.listdir("ptms"):
-  fpath = os.path.join("ptms", fname)
-  df = pd.read_csv(fpath, sep="\t", names=["name", "uniprot", "ptm_location", "ptm_name", "unknown_stupid", "adjacent_seq"])
+for fname in os.listdir(INPUTS_RAW_DIR):
+  logger.info(f"Processing raw file {fname}")
+  fpath_in = os.path.join(INPUTS_RAW_DIR, fname)
+  fpath_out = os.path.join(INPUTS_CLEAN_DIR, fname + ".csv")
+  df = pd.read_csv(fpath_in, sep="\t", names=["name", "uniprot", "ptm_location", "ptm_name", "unknown_stupid", "adjacent_seq"])
   full_seqs = []
   for _, row in tqdm(df.iterrows()):
     for _ in range(50):
@@ -54,4 +63,4 @@ for fname in os.listdir("ptms"):
       except:
         sleep(5)
   df["seq"] = full_seqs
-  df.to_csv(fpath, index=False)
+  df.to_csv(fpath_out, index=False)
