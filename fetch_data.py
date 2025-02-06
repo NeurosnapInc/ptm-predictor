b670124962d4c16e50650518f9e5f6123e38ac67
Keaun A <kja.working@gmail.com>
2025-02-05 20:09:11 -0500

    don't redownload raw if not required

--- fetch_data.py
+++ fetch_data.py
@@ -14,38 +14,46 @@ from tqdm import tqdm
 INPUTS_RAW_DIR = "ptms_raw"
 INPUTS_CLEAN_DIR = "ptms_clean"
 
+### Functions
+def download_raw():
+  ## download raw data
+  # prepare directory
+  shutil.rmtree(INPUTS_RAW_DIR, ignore_errors=True)
+  os.makedirs(INPUTS_RAW_DIR)
+
+  # fetch and parse download page
+  r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/download.php")
+  r.raise_for_status()
+  soup = bs4.BeautifulSoup(r.text)
+
+  # download each ptm file
+  for el in soup.select("#site .btn.btn-primary"):
+    if el.text == "MAC / Linux":
+      name = el["href"].split("/")[-1]
+      logger.info(f"Fetching {name}")
+      r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/" + el["href"])
+      r.raise_for_status()
+      tgz_path = os.path.join(INPUTS_RAW_DIR, name)
+
+      # Save the .tgz file
+      with open(tgz_path, "wb") as f:
+        f.write(r.content)
+
+      # Extract the .tgz file
+      with tarfile.open(tgz_path, "r:gz") as tar:
+        tar.extractall(INPUTS_RAW_DIR)
+
+      os.remove(tgz_path)
+
 ### Core Program
 ## prepare directories
-shutil.rmtree(INPUTS_RAW_DIR, ignore_errors=True)
-os.makedirs(INPUTS_RAW_DIR)
+# create raw files directory if it hasn't been fully downloaded yet
+if not os.path.exists(INPUTS_CLEAN_DIR):
+  download_raw()
 
+# create clean directory
 shutil.rmtree(INPUTS_CLEAN_DIR, ignore_errors=True)
 os.makedirs(INPUTS_CLEAN_DIR)
-
-## download raw data
-# fetch and parse download page
-r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/download.php")
-r.raise_for_status()
-soup = bs4.BeautifulSoup(r.text)
-
-# download each ptm file
-for el in soup.select("#site .btn.btn-primary"):
-  if el.text == "MAC / Linux":
-    name = el["href"].split("/")[-1]
-    logger.info(f"Fetching {name}")
-    r = requests.get("https://biomics.lab.nycu.edu.tw/dbPTM/" + el["href"])
-    r.raise_for_status()
-    tgz_path = os.path.join(INPUTS_RAW_DIR, name)
-
-    # Save the .tgz file
-    with open(tgz_path, "wb") as f:
-      f.write(r.content)
-
-    # Extract the .tgz file
-    with tarfile.open(tgz_path, "r:gz") as tar:
-      tar.extractall(INPUTS_RAW_DIR)
-
-    os.remove(tgz_path)
 
 ## process each raw TSV file
 # add full uniprot sequences to each dataframe
